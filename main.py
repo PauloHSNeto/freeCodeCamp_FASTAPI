b4be9c4412a269c29fa9e587e1b634f0577b4307
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-30 08:01:34 -0300

    itsworking :D

--- main.py
+++ main.py
@@ -33,12 +33,19 @@ class StudentModel(BaseModel):
     age:int
     isActive:bool
 
+
+class UpdateStudent(BaseModel):
+    name:Optional[str]=None
+    age:Optional [int]=None
+    isActive:Optional[bool]=None
+
+
 @api.get("/")
 def index():
     return{"name":"FirstData"}
 
 @api.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=5)):
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0)):
     return students[student_id]
 
 
@@ -51,8 +58,24 @@ def get_student(*,student_id: int,name:Optional[str]=None,test:int):
 
 
 @api.post("/create-student/{student_id}")
-def create_student(id:int, newStudent: StudentModel):
-    if id in students:
+def create_student(student_id:int, newStudent: StudentModel):
+    if student_id in students:
         return {"Wrong ID"}
-    students[id]= newStudent
-    return students[id]
+    students[student_id]= newStudent
+    return students[student_id]
+
+@api.put("/update-student/{student_id}")
+def update_student(student_id:int, student_to_update:UpdateStudent):
+    if student_id not in students:
+        return {"This id is not valid"}
+
+    if student_to_update.name != None:
+        students[student_id].name = student_to_update.name
+
+    if student_to_update.age != None:
+        students[student_id].age = student_to_update.age
+
+    if student_to_update.isActive != None:
+        students[student_id].isActive = student_to_update.isActive
+
+    return students[student_id]
